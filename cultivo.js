a58da43cf81d820e70c501083964a60e693e4059
Paulo McNally <paulomcnally@gmail.com>
2014-12-15 18:56:48 -0600

    added notify

--- cultivo.js
+++ cultivo.js
@@ -1,6 +1,46 @@
 /**
- * Class Cultivo
- */
+* Class Cultivo
+*/
 var Cultivo = function() {
 
+}
+
+Cultivo.prototype.notify = function(params) {
+  var params = params || {};
+  params.type = params.type || 'info';
+  params.title = params.title || 'Title';
+  params.message = params.message || 'Message';
+  var icon = '';
+  var theme = '';
+  switch (params.type) {
+    case 'info':
+      icon = 'fa fa-info';
+      theme = 'awesome blue';
+    break;
+    case 'danger':
+      icon = 'fa fa-times';
+      theme = 'awesome error';
+    break;
+    case 'warning':
+      icon = 'fa fa-exclamation';
+      theme = 'awesome yellow';
+    break;
+    case 'success':
+      icon = 'fa fa-check';
+      theme = 'awesome ok';
+    break;
+
+  }
+
+  $.amaran({
+    content: {
+      title: params.title,
+      message: params.message,
+      icon: icon,
+    },
+    theme: theme,
+    closeButton: true,
+    sticky: true,
+    position: 'top right'
+  });
 }
